77926ef62086af5d7c98d9c755330c108dc84e65
Chris Peralta <chris99994@gmail.com>
2019-03-30 17:45:03 +0800

    Finished pov data script

--- R/import_pov_data.R
+++ R/import_pov_data.R
@@ -4,11 +4,33 @@
 
 library(googlesheets)
 
-pov_spreadsheet <- gs_title("Malazan POV data")
-
 pov_data <- 
-  map_df(1:10, ~gs_read(pov_spreadsheet, ., col_names = TRUE, range = cell_cols(1:4))) %>%
-  janitor::clean_names()
+  map_df(1:10, 
+         function(x){
+           pov_spreadsheet <- gs_title("Malazan POV data")
+           gs_read(pov_spreadsheet, x, col_names = TRUE, range = cell_cols(1:4)) %>% 
+             mutate(book = x)
+   }
+  ) %>% 
+  janitor::clean_names() %>% 
+  rename(chapter = chapter_number, 
+         names = character) %>% 
+  mutate(chapter = if_else(chapter == "P", "Ch0", chapter), 
+         chapter = str_remove(chapter, "[A-Za-z]{2}"), 
+         chapter = if_else(chapter == "E1", "25", chapter), 
+         chapter = if_else(chapter == "E2", "26", chapter), 
+         chapter = if_else(chapter == "E" & (book == 3 | book == 5), "26", chapter), 
+         chapter = if_else(chapter == "E" & book == 4, "27", chapter), 
+         chapter = if_else(chapter == "E", "25", chapter), 
+         chapter = as.integer(chapter), 
+         names = if_else(str_detect(names, "Sorry") & book == 1, 
+                         "Sorry", 
+                         names), 
+         names = if_else(str_detect(names, "Sorry") & book != 1, 
+                         "Apsalar", 
+                         names)) %>% 
+  standardize_names_net() %>% 
+  filter(!is.na(order)) %>% 
+  rename(name = names)
 
-pov_data %>% 
-  rename()
+
